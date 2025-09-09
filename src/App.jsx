ed789d0f804787c834a979b5ab2180353ae4e0c0
Amala-Francis <amalaafranciss@gmail.com>
2025-09-09 13:25:03 +0530

    branch6

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Next branch</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,6 +27,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more1
+        Click on the Vite and React logos to learn more2
+        Click on the Vite and React logos to learn more3
       </p>
     </>
   )
